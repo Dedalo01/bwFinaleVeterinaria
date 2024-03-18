4c6fe496f813e4a0e8cf47e4914c9e4dac26c2fb
Gaetano <gaem195@gmail.com>
2024-03-18 18:00:18 +0100

    Add Controllers

---
+++ bwFinaleVeterinaria/Controllers/ApiController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace bwFinaleVeterinaria.Controllers
+{
+    public class ApiController : Controller
+    {
+        // GET: Api
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
