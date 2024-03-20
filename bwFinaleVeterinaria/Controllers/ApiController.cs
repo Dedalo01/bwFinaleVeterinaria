cb5abadca3c66d7457d311fc1cc734399c30d6e9
Gaetano <gaem195@gmail.com>
2024-03-20 20:01:58 +0100

    add working async fetch in Doctor Index

--- bwFinaleVeterinaria/Controllers/ApiController.cs
+++ bwFinaleVeterinaria/Controllers/ApiController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using bwFinaleVeterinaria.Models;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace bwFinaleVeterinaria.Controllers
 {
@@ -8,6 +10,38 @@ namespace bwFinaleVeterinaria.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public JsonResult GetActiveAdmissions()
+        {
+            VeterinariaDbContext db = new VeterinariaDbContext();
+
+            var strayPets = db.Pets
+             .Where(p => p.RescueAdmissions.Any(ra => ra.EndAdmissionDate == null))
+             .Select(p => new
+             {
+                 Pet = new
+                 {
+                     p.Id,
+                     p.Name,
+                     p.Type,
+                     p.CoatColor,
+                     p.Microchip
+                 },
+                 RescueAdmission = p.RescueAdmissions
+                     .Where(ra => ra.EndAdmissionDate == null)
+                     .Select(ra => new
+                     {
+                         ra.Id,
+                         ra.AdmissionDate,
+                         ra.PetImageUrl,
+                         ra.Price
+                     })
+                     .FirstOrDefault()
+             })
+             .ToList();
+
+            return Json(strayPets, JsonRequestBehavior.AllowGet);
         }
     }
 }
