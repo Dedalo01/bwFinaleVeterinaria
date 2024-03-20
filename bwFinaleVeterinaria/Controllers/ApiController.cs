34d75dbb43f0e642f91b868160378c90b06a4561
Abel Sassu <relinquished@hotmail.it>
2024-03-20 16:43:12 +0100

    feature async Ricerca per microchip + css

--- bwFinaleVeterinaria/Controllers/ApiController.cs
+++ bwFinaleVeterinaria/Controllers/ApiController.cs
@@ -1,13 +1,34 @@
 ﻿using System.Web.Mvc;
+using bwFinaleVeterinaria.Models;
+using System.Linq;
 
 namespace bwFinaleVeterinaria.Controllers
 {
     public class ApiController : Controller
     {
-        // GET: Api
-        public ActionResult Index()
+        private VeterinariaDbContext db = new VeterinariaDbContext();
+
+        public ActionResult GetPetByMicrochip(string microchip)
         {
-            return View();
+            var pet = db.Pets.Where(p => p.Microchip == microchip).Select(p => new
+            {
+                p.Name,
+                p.Microchip,
+                AdmissionInfo = db.RescueAdmissions.Where(ra => ra.PetId == p.Id).Select(ra => new
+                {
+                    ra.PetImageUrl,
+                    ra.AdmissionDate
+                }).FirstOrDefault()
+            }).FirstOrDefault();
+
+            if (pet != null)
+            {
+                return Json(new { success = true, pet = pet }, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(new { success = false }, JsonRequestBehavior.AllowGet);
+            }
         }
     }
 }
