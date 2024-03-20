b643ae5c06c50eb3ed40467b9cb06e700a1c34f8
marcod08 <marcodimpirma91@gmai.com>
2024-03-20 16:46:35 +0100

    Aggiunta chiamata asincrona

--- bwFinaleVeterinaria/Controllers/ApiController.cs
+++ bwFinaleVeterinaria/Controllers/ApiController.cs
@@ -1,13 +1,31 @@
-﻿using System.Web.Mvc;
+﻿using bwFinaleVeterinaria.Models;
+using System.Web.Mvc;
+using System.Data.Entity;
+using System.Linq;
+
 
 namespace bwFinaleVeterinaria.Controllers
 {
     public class ApiController : Controller
     {
+        private VeterinariaDbContext db = new VeterinariaDbContext();
+
         // GET: Api
         public ActionResult Index()
         {
             return View();
+        }
+
+        //chiamata asincrona per le Examination
+        public ActionResult GetAnimalHistory(int petId)
+        {
+            var animal = db.Pets.Include(p => p.Examinations).SingleOrDefault(p => p.Id == petId);
+            if (animal == null)
+            {
+                return HttpNotFound();
+            }
+
+            return PartialView("_AnimalHistoryPartial", animal.Examinations.OrderByDescending(e => e.ExaminationDate));
         }
     }
 }
