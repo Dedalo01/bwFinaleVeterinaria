26b3706960cdbaf19835215f0cc28afdfd7daf37
Gaetano <gaem195@gmail.com>
2024-03-20 12:23:02 +0100

    add functioning AddPet view, get and post

--- bwFinaleVeterinaria/Controllers/DoctorController.cs
+++ bwFinaleVeterinaria/Controllers/DoctorController.cs
@@ -1,4 +1,5 @@
 ﻿using bwFinaleVeterinaria.Models;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace bwFinaleVeterinaria.Controllers
@@ -9,8 +10,39 @@ namespace bwFinaleVeterinaria.Controllers
         // GET: Doctor
         public ActionResult Index()
         {
+            return View();
+        }
+
+        public ActionResult AddPet()
+        {
+            var owners = db.Owners.ToList();
+            TempData["owners"] = owners;
 
             return View();
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddPet(Pet pet, int ownerId)
+        {
+            if (ownerId < 0)
+            {
+                TempData["Error"] = "Non hai inserito il Proprietario";
+                return RedirectToAction("AddPet");
+            }
+
+            if (ModelState.IsValid)
+            {
+                pet.OwnerId = ownerId;
+                db.Pets.Add(pet);
+                db.SaveChanges();
+                TempData["Success"] = $"L'Animale di nome {pet.Name} è stato aggiunto al database con successo.";
+                return RedirectToAction("AddPet");
+            }
+
+            // error da modelState
+            return View(pet);
         }
     }
 }
