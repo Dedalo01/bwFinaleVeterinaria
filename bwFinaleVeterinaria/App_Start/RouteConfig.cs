340fc2ab9b2e39316b364dd180782eaf01c79d7a
MarcoV98 <148749261+MarcoV98@users.noreply.github.com>
2024-03-20 17:03:00 +0100

    prima parte search

--- bwFinaleVeterinaria/App_Start/RouteConfig.cs
+++ bwFinaleVeterinaria/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace bwFinaleVeterinaria
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+               name: "PharmacistSearch",
+               url: "Pharmacist/Search",
+               defaults: new { controller = "Pharmacist", action = "Search" }
+           );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
--- bwFinaleVeterinaria/Controllers/PharmacistController.cs
+++ bwFinaleVeterinaria/Controllers/PharmacistController.cs
@@ -1,17 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Data.Entity;
 using System.Linq;
-using System.Web;
+using System.Threading.Tasks;
 using System.Web.Mvc;
+using bwFinaleVeterinaria.Models;
 
 namespace bwFinaleVeterinaria.Controllers
 {
     public class PharmacistController : Controller
     {
-        // GET: Pharmacist
-        public ActionResult Index()
+        private readonly VeterinariaDbContext _context;
+
+        public PharmacistController()
         {
-            return View();
+            _context = new VeterinariaDbContext();
+        }
+
+        // GET: Pharmacist/Search
+        public ActionResult Search()
+        {
+            return View(); // Assicurati che esista una vista chiamata Search.cshtml nella cartella Views/Pharmacist
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Search(string productName)
+        {
+            var product = await _context.Products
+                .Include("Cabinets")
+                .Include("Cabinets.Drawer")
+                .FirstOrDefaultAsync(p => p.Name == productName);
+
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+
+            return PartialView("_ProductDetailsPartial", product);
         }
     }
 }
